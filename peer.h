6afa10381c31d486b875d3b2fc3e549d287e626d
Cody Schafer <cpschafer@gmail.com>
2011-05-09 23:12:31 -0400

    more data structures.

--- peer.h
+++ peer.h
@@ -1,6 +1,9 @@
 #ifndef PEER_H_
 #define PEER_H_
 
+/* struct sockaddr_storage */
+#include <netinet/in.h>
+
 /* XXX: presently assumes that each 'peer' has a single method of reaching it.
  * this is inaccurate */
 
@@ -12,13 +15,27 @@ enum peer_type {
 	PT_REST_PORT_NAT  /* restricted port */
 };
 
+struct peer_id {
+	/* KAD id for now (128 bit) */
+	uint8_t id[16];
+};
+
+/* XXX: this is a copy of addrinfo with route_type added. I don't like the
+ * duplication, but it isn't clear how to avoid it. */
+struct peer_addrinfo {
+	enum peer_route_type	 pai_route_type;
+	int			 pai_family;
+	int			 pai_socktype;
+	int			 pai_protocol;
+	size_t			 pai_addrlen;
+	struct sockaddr		*pai_addr;
+	char			*pai_canonname;
+	struct peer_addrinfo	*pai_next;
+};
+
 struct peer {
-	enum peer_type pt;
-
-	struct sockaddr_storage saddr;
-	int saddr_len;
-
-
+	struct peer_addrinfo *pai;
+	struct peer_id pid;
 };
 
 #endif
