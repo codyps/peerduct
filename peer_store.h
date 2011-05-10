b83b99902550d43a3f65e93d60b510ce03de7a2d
Cody Schafer <cpschafer@gmail.com>
2011-05-09 23:15:04 -0400

    note need for peer store.

--- peer_store.h
+++ peer_store.h
@@ -3,6 +3,8 @@
 
 /* XXX: need a way to load and store peers.
  * Bencoded/JSON/ed2k .met/xml?
+ * Maybe use sqlite or tokyocabinet? (do we need this? will data set be large
+ * and lookup times an important item?)
  */
 
 #endif
