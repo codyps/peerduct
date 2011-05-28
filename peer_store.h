ee01a2562fece2b1fe4d1a5a9b2b3f1378cf0dca
Cody Schafer <cpschafer@gmail.com>
2011-05-28 17:17:50 -0400

    nodes.dat is the file to target, added docs on it.

--- peer_store.h
+++ peer_store.h
@@ -2,30 +2,58 @@
 #define PEER_STORE_H_
 
 /* XXX: need a way to load and store peers.
- * Bencoded/JSON/ed2k .met/xml?
+ * Bencoded/JSON/ed2k .nd/xml?
  * Maybe use sqlite or tokyocabinet? (do we need this? will data set be large
  * and lookup times an important item?)
  */
 
-struct met_parse_ctx {
+
+/*
+ * nodes.dat (v2) parsing.
+ * 	used in amule >= 2.2.0 for KAD contacts.
+ * 	used in emule (version?) for KAD contacts.
+ *
+ * doc - http://wiki.amule.org/index.php/Nodes.dat_file
+ *
+ * all fields are LE.
+ */
+struct nd_header {
+	uint32_t zero;    /* always 0, used to differentiate from v1, which has nr
+			     here. */
+	uint32_t version; /* of the fileformat, always 2 */
+	uint32_t nr;      /* number of nodes in file */
+} __packed;
+
+struct nd_entry {
+	uint8_t client_id[16];
+	uint32_t ip_addr;
+	uint16_t tcp_port;
+	uint16_t udp_port;
+	uint8_t  version;  /* 0 = kad v1, non-zero indicates v2 and is a
+			      featured bitmask */
+	uint8_t  kad_udp_key[8]; 
+	uint8_t  verified; /* non-zero value indicates verified. */
+} __packed;
+
+struct nd_parse_ctx {
 };
 
 /**
- * met_parse_init - sets up the met_parse_ctx for processing.
+ * nd_parse_init - sets up the nd_parse_ctx for processing.
  *
- * @mpc - an uninitialized met_parse_ctx.
+ * @mpc - an uninitialized nd_parse_ctx.
  *
  * @return - 0 on success, -1 on failure.
  */
-int met_parse_init(struct met_parse_ctx *mpc);
+int nd_parse_init(struct nd_parse_ctx *mpc);
 
 /**
- * met_parse_proc - processes the given buffer. Indicates completion of the
+ * nd_parse_proc - processes the given buffer. Indicates completion of the
  *                  parse based on the amount it indicates it consumed.
  *
- * @mpc - an initialized met_parse_ctx.
- * @buf - a buffer containing met data (which may continue the met data
- *        passed in a previous call to met_parse_proc).
+ * @mpc - an initialized nd_parse_ctx.
+ * @buf - a buffer containing nd data (which may continue the nd data
+ *        passed in a previous call to nd_parse_proc).
  * @len - the length of @buf.
  *
  * @return - <0 on error. Otherwise, the number of bytes of buf read.
@@ -33,28 +61,26 @@ int met_parse_init(struct met_parse_ctx *mpc);
  *           to @len. If the parsing is completed, it will be some value
  *           less than @len.
  */
-int met_parse_proc(struct met_parse_ctx *mpc, char *buf, size_t len);
+int nd_parse_proc(struct nd_parse_ctx *mpc, char *buf, size_t len);
 
 /**
- * met_parse_get_peers - returns the current peers asociated with the met_parse_ctx,
+ * nd_parse_get_peers - returns the current peers asociated with the nd_parse_ctx,
  *                       and removes them from the ctx such that calling
- *                       met_parse_get_peers a second time without further
+ *                       nd_parse_get_peers a second time without further
  *                       parsing taking place will return 0 peers.
  *
  * @mpc - the context from which the peers are removed.
  *
  * @return - a heap allocated list of peers.
  */
-struct peer *met_parse_get_peers(struct met_parse_ctx *mpc);
+struct peer *nd_parse_get_peers(struct nd_parse_ctx *mpc);
 
 /**
- * met_parse_destroy - free any data allocated in init or the parsing process.
+ * nd_parse_destroy - free any data allocated in init or the parsing process.
  *
  * @mpc - ctx to be destroyed.
  */
-void met_parse_destroy(struct met_parse_ctx *mpc);
-
-
+void nd_parse_destroy(struct nd_parse_ctx *mpc);
 
 struct bencode_parse_state {
 };
@@ -63,8 +89,8 @@ struct bencode_parse_state {
  * load_peers_from_bencode - what torrent programs use bencode for
  *                           peer storage? Do they all have the same layout?
  * 
- * @start - first byte in the met file.
- * @end   - last byte in the met file.
+ * @start - first byte in the nd file.
+ * @end   - last byte in the nd file.
  * @p     - points to a valid struct peer * to fill in.
  *
  * @return - 0 on success, -1 on failure.
