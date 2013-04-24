ec939a34d947be8f7a8e2cc505a2cf0cc2c1ebbe
Cody P Schafer <jmesmon@gmail.com>
2013-04-23 20:20:32 -0700

    stuff.

--- bt_dht.c
+++ bt_dht.c
@@ -1,13 +1,16 @@
 /* Another kademlia variant */
 
+/* XXX: because bucket splitting is deterministic, there should be a way
+ * to track it without start & end. */
 struct bucket {
 	struct rb_node node;
-	unsigned min;
-	unsigned max;
+	int num_nodes;
+	int time_last_changed;
 };
 
 struct routing_table {
 	size_t bucket_ct;
+	size_t nodes_per_bucket; /* K */
 	struct rb_tree buckets;
 };
 
