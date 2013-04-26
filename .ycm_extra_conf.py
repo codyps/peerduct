22560ce181839a6821bd38e804d515fb9ffa79d8
Cody P Schafer <jmesmon@gmail.com>
2013-04-26 00:13:30 -0700

    fiddle.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -9,9 +9,6 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-fexceptions',
-'-DNDEBUG',
-'-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
