d9293fc3ebbd58cc8587425231e9c45bfa002d04
Cody P Schafer <dev@codyps.com>
2016-02-06 00:13:01 -0500

    notes

--- hornet.c
+++ hornet.c
@@ -27,3 +27,15 @@ struct hornet_fs {
 struct hornet_ahdr {
 
 };
+
+
+struct hornet {
+	unsigned k; /* security parameter, paper uses 128 */
+
+
+	/* G, a prime order cyclic group of order q (q ~ 2**(2k)), which satisfies the decisional dh assumption */
+	/* G*, a set of non-identity elements in G */
+	/* g, a generator of G */
+
+	unsigned r; /* maximum length of a path */
+};
